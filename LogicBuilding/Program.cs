f1b9d6cb58085fc8d9667bd3bf1e78589b562a56
Naveed Ullah <Naveed Ullah@DESKTOP-AFJJC1D>
2018-03-10 00:26:42 +0500

    Add project files.

---
+++ LogicBuilding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicBuilding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
