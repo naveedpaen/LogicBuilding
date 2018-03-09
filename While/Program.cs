d4b34726976170fbd00b490ef286937b0a212cb1
Naveed Ullah <Naveed Ullah@DESKTOP-AFJJC1D>
2018-03-10 00:43:12 +0500

    3

---
+++ While/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
