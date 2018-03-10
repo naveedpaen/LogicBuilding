05b8bfda831acadc9375a8a6a276fbf2a47a0a49
Naveed Ullah <Naveed Ullah@DESKTOP-AFJJC1D>
2018-03-10 20:39:39 +0500

    dd

--- Array/Program.cs
+++ Array/Program.cs
@@ -6,7 +6,7 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
