24cf853fea665063d95f5928b06dd57397793983
Naveed Ullah <Naveed Ullah@DESKTOP-AFJJC1D>
2018-03-12 01:57:19 +0500

    Array Changes

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-
+using Array;
 namespace Array
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            // different ways to declare array
             int[] dates = new int[29];
-
             int[] date = new int[] { 1,2,3};
-
             object[] dat = { 5, 5, 5 };
+
 
     //  Because the object type is the base type for all the types in .NET
             object[] da = new object[] { 2, 3, "t" };
@@ -23,15 +22,18 @@ namespace Array
             abc[0] = 4;
             Console.WriteLine(abc[1]);
             Console.WriteLine("\n");
-            
 
-            
 
-            
-            foreach (var item in da)
-            {
-                Console.WriteLine(item);
-            }
+
+            //...  Get value of any index No
+            Console.WriteLine("Get value of any index No");
+            int[] ArrayObj = new int[5] { 1, 2, 3, 4, 5 };
+            object a = ArrayObj.GetValue(3);
+            Console.WriteLine(a);
+
+
+
+
 
 
             Console.ReadKey();
