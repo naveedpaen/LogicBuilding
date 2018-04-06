3cf97c540c3101bf9bfa2a4b1f045a748be5c471
Naveed Ullah <Naveed Ullah@DESKTOP-AFJJC1D>
2018-04-06 11:44:12 +0500

    if

--- If/Program.cs
+++ If/Program.cs
@@ -6,7 +6,50 @@ namespace If
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int a = 2;
+
+            //if (a != 2)
+            //{
+            //    Console.WriteLine("Value of a is not 2");
+            //}
+
+            //else if (a == 2)
+            //{
+            //    Console.WriteLine("Value of a is 2");
+            //}
+
+
+            //int b = 4;
+
+            //if (b == 2)
+            //{
+            //    Console.WriteLine("Value of b is 2");
+            //}
+            //if (b==3)
+            //{
+            //    Console.WriteLine("Value of b is 3");
+            //}
+            Start:
+            Console.WriteLine("Please enter your two numbers:");
+            int a =Convert.ToInt32( Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Your Answer");
+            int result = Convert.ToInt32(Console.ReadLine());
+            if (result == a+b)
+            {
+                Console.WriteLine("Right ANswer Wao");
+            }
+            else
+            {
+                Console.WriteLine("Noooooooooo");
+            }
+            
+
+
+            Console.ReadKey();
+            Console.WriteLine();
+                goto Start;
         }
     }
 }
