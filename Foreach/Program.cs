182abe57b15e4ae6ee71ddf5c91895f2066605e3
Naveed Ullah Amin <orignaldiamond@hotmail.com>
2018-10-17 21:06:50 +0500

    acc

--- Foreach/Program.cs
+++ Foreach/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
 
 namespace Foreach
 {
+
+    class student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Mobile { get; set; }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            List<student> studentList = new List<student>()
+            {
+                new student{Id=1, Name="Naveed Ullah", Mobile=0303},
+                new student{Id=2, Name="Waheed Ullah", Mobile=0345},
+                new student{Id=3, Name="Ali", Mobile=0333},
+            };
+
+           // studentList.ForEach(x => x.Id);
+            Console.WriteLine();
+
+            foreach (var student in studentList)
+            {
+                Console.WriteLine($" Id= {student.Id} Name={student.Name} Mobile#{student.Mobile}");
+            }
+
+
+            studentList.ForEach(x => {Console.WriteLine($" Id= {x.Id} Name={x.Name} Mobile#{x.Mobile}");});
+            var si = studentList.Find(x=> x.Id == 1);
+            var se = studentList.Select(x => x.Id == 3);
+
+
+
+
+
+            ReadLine();
+
+
+
+                 
         }
     }
 }
---
+++ Lambda/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+
+    public class EmailSending
+    {
+        public int Id { get; set; }
+
+        public string TaskName { get; set; }
+
+        public String Description { get; set; }
+
+        public string TargetDate { get; set; }
+
+        public string SupportUserName { get; set; }
+
+        public string ToEmail { get; set; }
+
+        public string StatusName { get; set; }
+
+        public string PriorityName { get; set; }
+
+        public string PortalName { get; set; }
+
+        public string ProjectName { get; set; }
+
+        public int SId { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<EmailSending> myList = new List<EmailSending>()
+            {
+                new EmailSending{Id=1, PortalName="netharland", ProjectName="project 2" , StatusName="Low", PriorityName="Mediu,", SupportUserName="Adnan", TargetDate="12/12/2012", TaskName="Task 1", ToEmail="abc@gmail.com" , SId = 1 },
+                new EmailSending{Id=2, PortalName="uk", ProjectName="new Project" , StatusName="Pending", PriorityName="High", SupportUserName="Naveed", TargetDate="12/12/2019", TaskName="Task 2", ToEmail="naveed@gmail.com" , SId =2},
+                new EmailSending{Id=3, PortalName="netharland", ProjectName="project 2" , StatusName="Low", PriorityName="Mediu,", SupportUserName="Adnan", TargetDate="12/12/2012", TaskName="Task 3", ToEmail="abc@gmail.com" , SId = 1 },
+            };
+
+            var abc = myList.Find(x => x.SId == 1);
+
+            foreach (var item in myList)
+            {
+                Console.WriteLine($"{item.Id} {item.TaskName}");
+            }
+
+
+        }
+    }
+}
