6c5cca81b799dde9f47b5793b8fde4514ba90c9b
Naveed Ullah <Naveed Ullah@DESKTOP-7GA6B6Q>
2018-10-24 22:19:00 -0700

    over

---
+++ Method Overriding/Master.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Overriding
+{
+    abstract public class Master
+    {
+
+        public abstract int Multiply();
+
+
+        public virtual int Subtract()
+        {
+            return 2 - 1;
+        }
+
+
+
+
+        public int calculate()
+        {
+            return 2 + 2;
+        }
+
+
+
+
+
+    }
+}
---
+++ Method Overriding/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Method_Overriding
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            Teacher t = new Teacher();
+            t.BirthDay();
+            Console.WriteLine("Teacher");
+            Console.WriteLine(t.BirthDay());
+            t.Name();
+            Console.WriteLine(t.Name());
+            t.Money();
+            Console.WriteLine(t.Money());
+            t.calculate();
+            Console.WriteLine(t.calculate());
+            t.Subtract();
+            Console.WriteLine(t.Subtract());
+            t.Multiply();
+            Console.WriteLine();
+            Console.WriteLine( t.Multiply());
+
+
+
+
+            Console.WriteLine();
+
+            Student s = new Student();
+            s.BirthDay();
+            Console.WriteLine("Student");
+            Console.WriteLine(s.BirthDay());
+            s.Name();
+            Console.WriteLine(s.Name());
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+
+
+    }
+}
