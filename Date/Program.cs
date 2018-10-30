a445a655c4a603c79b4fb5739b5c2365830b2730
Naveed Ullah Amin <orignaldiamond@hotmail.com>
2018-10-30 11:59:22 +0500

    office

---
+++ Date/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Timers;
+
+namespace Date
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer t = new Timer();                
+            t.Enabled = true;
+            t.Interval = 5000;
+            t.Elapsed += new ElapsedEventHandler(check);
+            t.Start();
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+
+
+        public static void check(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Time finished");
+        }
+    }
+}
