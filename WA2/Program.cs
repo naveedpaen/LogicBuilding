2a7fc70335be047d79a538e682ab111cd5b3e7fb
Naveed Ullah <Naveed Ullah@DESKTOP-7GA6B6Q>
2018-12-08 07:33:34 +0500

    send

---
+++ WA2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WA2
+{
+    class Program
+    {
+
+        // Parameters   1. low A  2. Medium B  3. High C
+        // 
+        public static string mymethod(string a)
+        {
+            return "";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
