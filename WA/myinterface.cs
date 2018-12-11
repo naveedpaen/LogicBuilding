351cd13b6d95235ec691f8996e6cdfb04bbdf902
Naveed Ullah <Naveed Ullah@DESKTOP-7GA6B6Q>
2018-12-11 08:22:39 +0500

    n

---
+++ WA/myinterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WA
+{
+    interface myinterface
+    {
+
+        int add();
+    }
+}
---
+++ WA/test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WA
+{
+    class test : myinterface
+    {
+        //public int add()
+        //{
+        //    throw new NotImplementedException();
+        //}
+       public int add()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
