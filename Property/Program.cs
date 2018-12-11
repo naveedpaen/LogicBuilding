fecc8ed246e852f95e4db763056d1bec5e429c3e
Naveed Ullah <Naveed Ullah@DESKTOP-7GA6B6Q>
2018-12-11 11:47:32 +0500

    new

---
+++ Property/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
