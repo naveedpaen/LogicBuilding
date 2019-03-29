8cc4b46f3040bf4e2a6c583610067e9b68c0cd44
Naveed Ullah <31434159+naveedpaen@users.noreply.github.com>
2019-03-29 12:25:17 +0500

    d

---
+++ Testing3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Testing3
+{
+
+    public class test {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+       public  int c;
+    }
+
+    
+
+    class Program
+    {
+        int b;
+        static void Main(string[] args)
+        {
+            int a;
+            test t = new test();
+            t.Name = "asif";
+            int b = 30;
+          int  sum = a + b;
+            Program p = new Program();
+
+            Console.WriteLine(t.c);
+
+            Console.ReadKey();
+        }
+    }
+}
